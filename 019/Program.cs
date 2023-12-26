9198014389dbf1873b6a3c1269f01c73a1512dd4
Asya <shulga-a.91@live.ru>
2023-12-26 14:50:01 +0700

    019

---
+++ 019/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char
+// [,]). Создать строку из символов этого массива. 
+
+// a b c => “abcdef”
+// d e f 
+
+
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        char[,] charArray = {
+    { 'A', 'B', 'C', 'D' },
+    { 'E', 'F', 'G', 'H' },
+    { 'I', 'J', 'K', 'L' }
+};
+        string str = "";
+        StringBuilder strB = new StringBuilder();
+        for (int i = 0; i < charArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < charArray.GetLength(1); j++)
+            {
+                strB.Append(charArray[i,j]);
+            }
+        }
+        Console.WriteLine($"{strB.ToString()}" + " ");
+    }
+}
