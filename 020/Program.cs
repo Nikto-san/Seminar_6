90dc6cc4af62a4e2d7fa6621a0f544852f04cd46
Asya <shulga-a.91@live.ru>
2023-12-27 10:52:48 +0700

    020

---
+++ 020/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные. 
+
+using System.Text;
+
+string str = "aBCdefJHij";
+Console.WriteLine($"Исходная строка: {str}");
+
+string reverseStr = ToOppositeCase(str);
+Console.WriteLine($"Изменная строка: {reverseStr}");
+
+string ToOppositeCase(string str)
+{
+    char[] array = new char[str.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (char.IsLower(str[i]))
+        {
+            array[i] = char.ToUpper(str[i]);
+        }
+        else if (char.IsUpper(str[i]))
+        {
+            array[i] = char.ToLower(str[i]);
+        }
+    }
+    return new string(array);
+}
